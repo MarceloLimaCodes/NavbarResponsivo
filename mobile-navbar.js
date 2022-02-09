8f6291ce360544e5151f76ded0738d633216e61c
MarceloLimaCodes <marcelolima557@gmail.com>
2022-02-09 15:05:48 -0300

    first commit, o botão do menu-mobile ainda não está funcional mas a estrutura está pronta

---
+++ mobile-navbar.js
@@ -0,0 +1,29 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("botão está funcionando"));
+
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+
+        return this
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init();
